5af84adcdd21fdfa40e5139779f4c1e5b0d704c5
Derek Vance <dacrepair@gmail.com>
2024-10-04 16:38:18 +0000

    Updated perms

--- tf/discord_roles.tf
+++ tf/discord_roles.tf
@@ -1,4 +1,38 @@
 ########################################################################################################################
+# Everyone
+########################################################################################################################
+data "discord_permission" "everyone" {
+  read_message_history  = "allow"
+  view_channel          = "allow"
+  add_reactions         = "allow"
+  attach_files          = "allow"
+  create_public_threads = "allow"
+  embed_links           = "allow"
+
+  send_messages            = "allow"
+  send_thread_messages     = "allow"
+  send_voice_messages      = "allow"
+  use_application_commands = "allow"
+  use_external_apps        = "allow"
+  use_external_emojis      = "allow"
+  use_external_stickers    = "allow"
+  use_external_sounds      = "allow"
+
+  connect                   = "allow"
+  speak                     = "allow"
+  stream                    = "allow"
+  start_embedded_activities = "allow"
+  use_soundboard            = "allow"
+  use_vad                   = "allow"
+}
+
+resource "discord_role_everyone" "everyone" {
+  permissions = data.discord_permission.everyone.allow_bits
+  server_id   = discord_server.production.id
+}
+
+
+########################################################################################################################
 # Master Baiters
 ########################################################################################################################
 data "discord_permission" "administrators" {
@@ -6,7 +40,7 @@ data "discord_permission" "administrators" {
 }
 
 data "discord_color" "administrators" {
-  hex = "#FC0FC0"
+  hex = "#FFD700"
 }
 
 resource "discord_role" "administrators" {
@@ -18,5 +52,35 @@ resource "discord_role" "administrators" {
   position    = 1
 
   permissions = data.discord_permission.administrators.allow_bits
+  server_id   = discord_server.production.id
+}
+
+
+########################################################################################################################
+# Hang Danglers
+########################################################################################################################
+data "discord_permission" "moderators" {
+  view_channel     = "allow"
+  mention_everyone = "allow"
+  manage_messages  = "allow"
+  manage_threads   = "allow"
+  kick_members     = "allow"
+  mute_members     = "allow"
+  ban_members      = "allow"
+}
+
+data "discord_color" "moderators" {
+  hex = "#FC0FC0"
+}
+
+resource "discord_role" "moderators" {
+  name  = "Hang Danglers"
+  color = data.discord_color.moderators.dec
+
+  hoist       = true
+  mentionable = false
+  position    = 2
+
+  permissions = data.discord_permission.moderators.allow_bits
   server_id   = discord_server.production.id
 }
